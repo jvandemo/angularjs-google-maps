b751da94a8f36511b4bafa83b0d5ae4ea3eb71a4
Jurgen Van de Moere <jurgen@webberid.com>
2013-09-12 18:29:54 +0200

    Added dist build

--- dist/angularjs-google-maps.js
+++ dist/angularjs-google-maps.js
@@ -7,14 +7,39 @@
 // Config
 angular.module('gm.config', [])
     .value('gm.config', {
-        debug: true
-    });
+        debug: true,
+        placess: {
+            autocomplete: {}
+        }
+    })
+    .run(['$window', function($window){
+
+        // Check for dependencies
+        if(
+            angular.isUndefined($window.google) ||
+            angular.isUndefined($window.google.maps)
+            ){
+            throw new Error('Google Maps API not available, please make sure the Google Maps library is loaded before the AngularJS Google Maps library is loaded');
+        }
+    }]);
 
 // Places modules
 angular.module('gm.places.directives', []);
 angular.module('gm.places', [
     'gm.places.directives'
-]);
+])
+    .run(['$window', function($window){
+
+        // Check for dependencies
+        if(
+            angular.isUndefined($window.google) ||
+            angular.isUndefined($window.google.maps) ||
+            angular.isUndefined($window.google.maps.places) ||
+            angular.isUndefined($window.google.maps.places.Autocomplete)
+            ){
+            throw new Error('Google Maps API not available, please make sure the Google Maps library is loaded before the AngularJS Google Maps library is loaded');
+        }
+    }]);
 
 // Modules
 angular.module('gm.directives', []);
@@ -27,4 +52,53 @@ angular.module('gm', [
     'gm.services',
     'gm.places'
 ]);
-})(window, document);
+angular.module('gm.places')
+    .directive('gmPlacesAutocomplete', ['$window', 'gm.config', function($window, gmConfig){
+
+        var configOptions = (gmConfig.places && gmConfig.places.autocomplete) || {};
+
+        console.log('Using config options', configOptions);
+
+        return {
+            restrict: 'AEC',
+            require : ['gmPlacesAutocomplete', '?ngModel'],
+            controller: ['$scope', '$element', '$attrs', '$transclude', function($scope, $element, $attrs, $transclude){
+
+                // Assemble options
+                var options = angular.extend({}, configOptions, $scope.$eval($attrs.gmOptions));
+
+                var element = $element[0];
+
+                this._autocomplete = undefined;
+
+                Object.defineProperties(this, {
+                    api: {
+                        get: function(){
+                            return this._autocomplete;
+                        }
+                    }
+                });
+
+                try {
+                    this._autocomplete = new google.maps.places.Autocomplete(element);
+                }
+                catch (err)
+                {
+                    if(gmConfig.debug) console.log('Could not instantiate autocomplete directive: ' + err.message);
+                }
+
+            }],
+            link: function(scope, iElement, iAttrs, controllers){
+
+                // Define controllers
+                var gmPlacesAutocompleteController = controllers[0];
+                var ngModelController = controllers[1];
+
+                // Update model if there is one
+                if (ngModelController) {
+                    ngModelController.$setViewValue(gmPlacesAutocompleteController);
+                }
+            }
+        };
+
+    }]);})(window, document);
