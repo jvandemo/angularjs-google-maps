84d2cdc6d267ec756cb46b68892f7a762d6f0b7b
Jurgen Van de Moere <jurgen@webberid.com>
2013-09-13 11:54:52 +0200

    Updated dist build

--- dist/angularjs-google-maps.js
+++ dist/angularjs-google-maps.js
@@ -52,12 +52,38 @@ angular.module('gm', [
     'gm.services',
     'gm.places'
 ]);
-angular.module('gm.places')
-    .directive('gmPlacesAutocomplete', ['$rootScope', 'gm.config', function($rootScope, gmConfig){
+/**
+ * Logger service
+ */
+angular.module('gm.services')
+    .factory('logger', ['$log', '$window', function ($log, $window) {
+
+        // Create service
+        var service = {};
+
+        // Proxy regular methods to $log
+        angular.forEach(['log', 'info', 'warn', 'error'], function(method){
+            service[method] = function(){
+                return $log[method](arguments);
+            };
+        });
+
+
+        // Add dir method to hierarchically display objects
+        service.dir = function (obj, title) {
+            if ($window.console) {
+                if (angular.isDefined(title)) {
+                    $log.info(title + ':');
+                }
+                $window.console.dir(obj);
+            }
+        };
+
+        return service;
+    }]);angular.module('gm.places')
+    .directive('gmPlacesAutocomplete', ['$rootScope', 'gm.config', 'logger', function($rootScope, gmConfig, logger){
 
         var configOptions = (gmConfig.places && gmConfig.places.autocomplete) || {};
-
-        console.log('Using config options', configOptions);
 
         return {
             restrict: 'AEC',
@@ -89,7 +115,7 @@ angular.module('gm.places')
                 }
                 catch (err)
                 {
-                    if(gmConfig.debug) console.log('Could not instantiate gmPlacesAutocomplete directive: ' + err.message);
+                    if(gmConfig.debug) logger.log('Could not instantiate gmPlacesAutocomplete directive: ' + err.message);
                 }
 
             }],
