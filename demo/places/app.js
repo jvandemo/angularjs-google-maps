44fcff3acdac094598d6717370f02377980f8c49
Jurgen Van de Moere <jurgen@webberid.com>
2013-09-13 11:41:46 +0200

    Added event listener to places autocomplete example

--- demo/places/app.js
+++ demo/places/app.js
@@ -1,4 +1,12 @@
 angular.module('places', ['gm'])
-    .controller('appCtrl', function(){
+    .controller('appCtrl', [function(){
         console.log('appCtrl loaded');
-    })
+    }])
+    .controller('placesAutocompleteCtrl', ['$scope', function($scope){
+        console.log('placesAutocompleteCtrl loaded');
+        $scope.$on('gmPlacesAutocomplete::placeChanged', function(){
+            console.log('Place changed');
+            console.dir(arguments);
+        })
+
+    }]);
